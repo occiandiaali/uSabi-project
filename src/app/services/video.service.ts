6c259bf58d0150fed21f4dc7634b77ec7eb1de8e
occiandiaali <ocean.diaali@gmail.com>
2023-10-16 16:33:48 +0100

    Style for fullscreen video

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -5,7 +5,9 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class VideoService {
-  private url = 'https://fakestoreapi.com/products?limit=5';
+  // private url = 'https://fakestoreapi.com/products?limit=5';
+  private url =
+    'https://gist.githubusercontent.com/poudyalanil/ca84582cbeb4fc123a13290a586da925/raw/14a27bd0bcd0cd323b35ad79cf3b493dddf6216b/videos.json';
 
   constructor(private httpClient: HttpClient) {}
 
